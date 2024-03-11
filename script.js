a5f54be92788e0c4e0f31f3cef6826fc95316dff
PradeepTeppala <pradeepteppala1212@gmail.com>
2024-03-11 18:46:42 +0530

    Add all changes

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
- 
+const apiKey = "ac20f0720732469f9cdc2de6bae66bd5";
+
+const blogContainer = document.getElementById("blog-container");
+
+ async function fetchRandomNews()  {
+    try{ 
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+    } catch(error){
+        console.error("Error fetching random  news", error);
+        return[];
+    }
+}
+
+function displayBlogs(articles){
+    blogContainer.innerHTML = "";
+    articles.forEach((article) =>{
+        const blogcard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+        img.alt = article.title;
+        const title = document.createElement("h2");
+        title.textContent = article.title;
+        const description = document.createElement("p");
+        description.textContent = article.description;
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogContainer.appendChild(blogCard);
+    });
+}
+
+(async () => {
+    try {
+    const articles = await fetchRandomNews(); 
+    displayBlogs(articles);
+    } catch (error) {
+        console.error("Error fetching random news", error);
+    }
+ })();
