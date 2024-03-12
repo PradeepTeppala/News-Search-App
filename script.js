3c8af999211fc2318d389fc233166082c72996ff
PradeepTeppala <pradeepteppala1212@gmail.com>
2024-03-12 09:57:46 +0530

    Add changes in script

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-const apiKey = "ac20f0720732469f9cdc2de6bae66bd5";
+const apikey = "ac20f0720732469f9cdc2de6bae66bd5";
 
 const blogContainer = document.getElementById("blog-container");
+const searchField = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 
- async function fetchRandomNews()  {
-    try{ 
-        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
+async function fetchRandomNews(){
+    try{
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
-        return data.articles;
+        return  data.articles;
     } catch(error){
-        console.error("Error fetching random  news", error);
+        console.error("Error fetching random news", error);
+        return[];
+    }
+}
+
+searchButton.addEventListener("click", async () =>{
+    const query = searchField.value.trim();
+    if(query !== ""){
+        try{
+            const articles = await fetchNewsquery(query);
+            displayBlogs(articles);
+        } catch(error){
+            console.log("Error fetching news by query", error);
+        }
+    }
+})
+
+async function fetchNewsquery(query){
+    try{
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apikey=${apikey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return  data.articles;
+    } catch(error){
+        console.error("Error fetching random news", error);
         return[];
     }
 }
@@ -17,28 +43,39 @@ const blogContainer = document.getElementById("blog-container");
 function displayBlogs(articles){
     blogContainer.innerHTML = "";
     articles.forEach((article) =>{
-        const blogcard = document.createElement("div");
+        const blogCard = document.createElement("div");
         blogCard.classList.add("blog-card");
         const img = document.createElement("img");
         img.src = article.urlToImage;
         img.alt = article.title;
         const title = document.createElement("h2");
-        title.textContent = article.title;
+        const truncatedTitle = 
+        article.title.length > 30 
+        ? article.title.slice(0, 30) + "...." 
+        :  article.title;
+    title.textContent = truncatedTitle;    
         const description = document.createElement("p");
-        description.textContent = article.description;
+        const truncatedDes = 
+        article.description.length > 120
+        ? article.description.slice(0, 120) + "...." 
+        :  article.description;
+        description.textContent = truncatedDes; 
 
         blogCard.appendChild(img);
         blogCard.appendChild(title);
         blogCard.appendChild(description);
+        blogCard.addEventListener('click', ()=>{
+            window.open(article.url, "_blank")
+        })
         blogContainer.appendChild(blogCard);
-    });
+    })
 }
 
-(async () => {
+(async () => { 
     try {
-    const articles = await fetchRandomNews(); 
-    displayBlogs(articles);
-    } catch (error) {
+        const articles = await fetchRandomNews();
+        displayBlogs(articles);
+    } catch(error){
         console.error("Error fetching random news", error);
     }
- })();
+})();
